a0ef51d37e115881899586a52d76a590a18f8de6
Nasif Mahbub <nasif.mahbub@enosisbd.com>
2019-12-19 12:07:12 +0600

    changed base.html to support same look across entire website. provided login_required decorator to deny unauthorized users to access website information via manual url.

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 # users/urls.py
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
     url(r'signup/', views.SignUpView.as_view(), name='signup'),
-    url(r'edit/(?P<pk>[0-9]+)/', views.EditView.as_view(), name='edit'),
+    url(r'edit/(?P<pk>[0-9]+)/', login_required(views.EditView.as_view()), name='edit'),
 ]
