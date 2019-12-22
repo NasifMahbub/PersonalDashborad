2a40bc035e66402df50b0e022035ce912e7424b1
Nasif Mahbub <nasif.mahbub@enosisbd.com>
2019-12-22 18:48:30 +0600

    tested for various situations.. minor updates

--- users/forms.py
+++ users/forms.py
@@ -10,7 +10,19 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         fields = ('username', 'first_name', 'last_name', 'email', 'contact_no', 'image')
 
-class CustomUserChangeForm(UserChangeForm, forms.ModelForm):
+    def save(self, commit=True):
+            user = super(CustomUserCreationForm, self).save(commit=False)
+            user.first_name = self.cleaned_data['first_name']
+            user.last_name = self.cleaned_data['last_name']
+            user.email = self.cleaned_data['email']
+            user.image = self.cleaned_data['image']
+
+            if commit:
+                user.save()
+
+            return user
+
+class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = CustomUser
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 # users/urls.py
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import login
 from . import views
 
 urlpatterns = [
-    url(r'signup/', views.SignUpView.as_view(), name='signup'),
-    url(r'edit/(?P<pk>[0-9]+)/', login_required(views.EditView.as_view()), name='edit'),
+    url(r'^login/', login, {'template_name': 'registration/login.html'}),
+    url(r'^signup/', views.SignUpView.as_view(), name='signup'),
+    url(r'^edit/(?P<pk>[0-9]+)/', login_required(views.EditView.as_view()), name='edit'),
 ]
