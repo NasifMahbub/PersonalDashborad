d18660cf61de4c5cecdce453ccdbe7ffdfbca761
Nasif Mahbub <nasif.mahbub@enosisbd.com>
2019-12-22 11:53:36 +0600

    hide image name in home page view.

--- users/forms.py
+++ users/forms.py
@@ -8,12 +8,10 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = CustomUser
-        image= forms.ImageField(label = 'Choose your image',  help_text = 'The image should be cool.')
         fields = ('username', 'first_name', 'last_name', 'email', 'contact_no', 'image')
 
 class CustomUserChangeForm(UserChangeForm, forms.ModelForm):
 
     class Meta:
         model = CustomUser
-        image= forms.ImageField(label = 'Choose your image',  help_text = 'The image should be cool.')
         fields = ('username', 'first_name', 'last_name', 'email', 'contact_no', 'password', 'image')
